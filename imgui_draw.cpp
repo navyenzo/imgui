be50eb86082b3c2747e9f336dd3fa407ca91a46c
thedmd <michcic@gmail.com>
2017-12-21 01:26:21 +0100

    Add stack based transformation to draw lists.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -7,6 +7,7 @@ Index of this file:
 
 // [SECTION] STB libraries implementation
 // [SECTION] Style functions
+// [SECTION] ImMatrix
 // [SECTION] ImDrawList
 // [SECTION] ImDrawListSplitter
 // [SECTION] ImDrawData
@@ -340,6 +341,38 @@ void ImGui::StyleColorsLight(ImGuiStyle* dst)
 }
 
 //-----------------------------------------------------------------------------
+// ImMatrix
+//-----------------------------------------------------------------------------
+ImMatrix ImMatrix::Inverted() const
+{
+    const float d00 = m11;
+    const float d01 = m01;
+
+    const float d10 = m10;
+    const float d11 = m00;
+
+    const float d20 = m10 * m21 - m11 * m20;
+    const float d21 = m00 * m21 - m01 * m20;
+
+    const float d = m00 * d00 - m10 * d01;
+
+    const float invD = d ? 1.0f / d : 0.0f;
+
+    return ImMatrix(
+         d00 * invD, -d01 * invD,
+        -d10 * invD,  d11 * invD,
+         d20 * invD, -d21 * invD);
+}
+
+ImMatrix ImMatrix::Rotation(float angle)
+{
+    const float s = sinf(angle);
+    const float c = cosf(angle);
+
+    return ImMatrix(c, s, -s, c, 0.0f, 0.0f);
+}
+
+//-----------------------------------------------------------------------------
 // ImDrawList
 //-----------------------------------------------------------------------------
 
@@ -373,6 +406,9 @@ void ImDrawList::Clear()
     _TextureIdStack.resize(0);
     _Path.resize(0);
     _Splitter.Clear();
+    _TransformationStack.resize(0);
+    _InvTransformationScale = 1.0f;
+    _HalfPixel = ImVec2(0.5f, 0.5f);
 }
 
 void ImDrawList::ClearFreeMemory()
@@ -387,6 +423,7 @@ void ImDrawList::ClearFreeMemory()
     _TextureIdStack.clear();
     _Path.clear();
     _Splitter.ClearFreeMemory();
+    _TransformationStack.clear();
 }
 
 ImDrawList* ImDrawList::CloneOutput() const
@@ -516,6 +553,82 @@ void ImDrawList::PopTextureID()
     UpdateTextureID();
 }
 
+void ImDrawList::SetTransformation(const ImMatrix& transformation)
+{
+    if (!_TransformationStack.empty())
+    {
+        ImMatrix finalTransformation;
+
+        for (ImDrawTransformation& transient : _TransformationStack)
+            finalTransformation = ImMatrix::Combine(transient.Transformation, finalTransformation);
+        finalTransformation = finalTransformation.Inverted();
+        finalTransformation = ImMatrix::Combine(transformation, finalTransformation);
+        ApplyTransformation(finalTransformation);
+    }
+    else
+        ApplyTransformation(transformation);
+}
+
+void ImDrawList::ApplyTransformation(const ImMatrix& transformation)
+{
+    ImDrawTransformation tr;
+    tr.Transformation             = transformation;
+    tr.VtxStartIdx                = _VtxCurrentIdx;
+    tr.LastInvTransformationScale = _InvTransformationScale;
+    tr.LastHalfPixel              = _HalfPixel;
+    _TransformationStack.push_back(tr);
+
+    const float scaleX = sqrtf(
+        transformation.m00 * transformation.m00 +
+        transformation.m01 * transformation.m01);
+    const float signX = (transformation.m00) < 0.0f ? -1.0f : 1.0f;
+
+    const float scaleY = sqrtf(
+        transformation.m10 * transformation.m10 +
+        transformation.m11 * transformation.m11);
+    const float signY = (transformation.m11) < 0.0f ? -1.0f : 1.0f;
+
+    const float scale = (scaleX + scaleY) * 0.5f;
+
+    const float invScale = scale > 0.0f ? (1.0f / scale) : 1.0f;
+
+    _InvTransformationScale = _InvTransformationScale * invScale;
+    _HalfPixel              = ImVec2(
+        _HalfPixel.x * signX * invScale,
+        _HalfPixel.y * signY * invScale);
+}
+
+void ImDrawList::PopTransformation(int count)
+{
+    IM_ASSERT(_TransformationStack.Size > 0);
+
+    for (int i = 0; i < count; ++i)
+    {
+        const ImDrawTransformation& tr = _TransformationStack.back();
+        if (tr.VtxStartIdx < _VtxCurrentIdx)
+        {
+            const ImMatrix& m = tr.Transformation;
+
+            ImDrawVert* const vertexBegin = VtxBuffer.Data + tr.VtxStartIdx;
+            ImDrawVert* const vertexEnd   = VtxBuffer.Data + _VtxCurrentIdx;
+
+            for (ImDrawVert* vertex = vertexBegin; vertex != vertexEnd; ++vertex)
+            {
+                const float x = vertex->pos.x;
+                const float y = vertex->pos.y;
+
+                vertex->pos.x = m.m00 * x + m.m10 * y + m.m20;
+                vertex->pos.y = m.m01 * x + m.m11 * y + m.m21;
+            }
+        }
+
+        _InvTransformationScale = tr.LastInvTransformationScale;
+        _HalfPixel              = tr.LastHalfPixel;
+
+        _TransformationStack.pop_back();
+    }
+}
+
 // NB: this can be called with negative count for removing primitives (as long as the result does not underflow)
 void ImDrawList::PrimReserve(int idx_count, int vtx_count)
 {
@@ -602,11 +715,13 @@ void ImDrawList::AddPolyline(const ImVec2* points, const int points_count, ImU32
     if (!closed)
         count = points_count-1;
 
-    const bool thick_line = thickness > 1.0f;
+    thickness *= _InvTransformationScale;
+
+    const bool thick_line = (fabsf(thickness - 1.0f) > FLT_EPSILON) || (fabsf(_InvTransformationScale - 1.0f) > FLT_EPSILON);
     if (Flags & ImDrawListFlags_AntiAliasedLines)
     {
         // Anti-aliased stroke
-        const float AA_SIZE = 1.0f;
+        const float AA_SIZE = _InvTransformationScale;
         const ImU32 col_trans = col & ~IM_COL32_A_MASK;
 
         const int idx_count = thick_line ? count*18 : count*12;
@@ -789,7 +904,8 @@ void ImDrawList::AddConvexPolyFilled(const ImVec2* points, const int points_coun
     if (Flags & ImDrawListFlags_AntiAliasedFill)
     {
         // Anti-aliased Fill
-        const float AA_SIZE = 1.0f;
+        const float DIRECTION = (((_HalfPixel.x < 0.0f) ^ (_HalfPixel.y < 0.0f)) ? -1.0f : 1.0f);
+        const float AA_SIZE = _InvTransformationScale * DIRECTION;
         const ImU32 col_trans = col & ~IM_COL32_A_MASK;
         const int idx_count = (points_count-2)*3 + points_count*6;
         const int vtx_count = (points_count*2);
@@ -813,8 +929,8 @@ void ImDrawList::AddConvexPolyFilled(const ImVec2* points, const int points_coun
             float dx = p1.x - p0.x;
             float dy = p1.y - p0.y;
             IM_NORMALIZE2F_OVER_ZERO(dx, dy);
-            temp_normals[i0].x = dy;
-            temp_normals[i0].y = -dx;
+            temp_normals[i0].x = dy * DIRECTION;
+            temp_normals[i0].y = -dx * DIRECTION;
         }
 
         for (int i0 = points_count-1, i1 = 0; i1 < points_count; i0 = i1++)
@@ -972,8 +1088,8 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 {
     if ((col & IM_COL32_A_MASK) == 0)
         return;
-    PathLineTo(p1 + ImVec2(0.5f, 0.5f));
-    PathLineTo(p2 + ImVec2(0.5f, 0.5f));
+    PathLineTo(p1 + _HalfPixel);
+    PathLineTo(p2 + _HalfPixel);
     PathStroke(col, false, thickness);
 }
 
@@ -984,9 +1100,9 @@ void ImDrawList::AddRect(const ImVec2& p_min, const ImVec2& p_max, ImU32 col, fl
     if ((col & IM_COL32_A_MASK) == 0)
         return;
     if (Flags & ImDrawListFlags_AntiAliasedLines)
-        PathRect(p_min + ImVec2(0.50f,0.50f), p_max - ImVec2(0.50f,0.50f), rounding, rounding_corners);
+        PathRect(p_min + _HalfPixel, p_max - _HalfPixel, rounding, rounding_corners);
     else
-        PathRect(p_min + ImVec2(0.50f,0.50f), p_max - ImVec2(0.49f,0.49f), rounding, rounding_corners); // Better looking lower-right corner and rounded non-AA shapes.
+        PathRect(p_min + _HalfPixel, p_max - ImVec2(_HalfPixel.x * 0.98f, _HalfPixel.y * 0.98f), rounding, rounding_corners); // Better looking lower-right corner and rounded non-AA shapes.
     PathStroke(col, true, thickness);
 }
 
@@ -1075,7 +1191,7 @@ void ImDrawList::AddCircle(const ImVec2& center, float radius, ImU32 col, int nu
 
     // Because we are filling a closed shape we remove 1 from the count of segments/points
     const float a_max = (IM_PI * 2.0f) * ((float)num_segments - 1.0f) / (float)num_segments;
-    PathArcTo(center, radius - 0.5f, 0.0f, a_max, num_segments - 1);
+    PathArcTo(center, radius - _HalfPixel.x, 0.0f, a_max, num_segments - 1);
     PathStroke(col, true, thickness);
 }
 
